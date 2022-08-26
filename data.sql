c7461cb44dc93b3afcc981970152945de0b32cd4
c3c1l1a <cesswangumukima@gmail.com>
2022-08-26 22:49:20 +0800

    Add owners_id information

--- data.sql
+++ data.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23),
-			 ('Ganumon', '2018-11-15', 2, TRUE, 8),
+			 ('Gabumon', '2018-11-15', 2, TRUE, 8),
 			 ('Pikachu', '2021-1-7', 1, FALSE, 15.04),
 			 ('Devimon', '2017-05-12', 5, TRUE, 11 );
 
@@ -50,6 +50,41 @@ SELECT * from animals;
 COMMIT;
 
 /* Update the owner_id in the animals table */
+BEGIN;
+UPDATE animals 
+SET owners_id = sam.id
+FROM (SELECT id from owners WHERE owners.full_name='Sam Smith') AS sam
+WHERE animals.name='Agumon';
+SELECT * FROM animals;
+
+UPDATE animals
+SET owners_id = jenifer.id 
+FROM (SELECT id from owners WHERE owners.full_name='Jennifer Orwell') AS jenifer
+WHERE animals.name = 'Gabumon' OR animals.name='Pikachu';
+SELECT * FROM animals;
+
+UPDATE animals
+SET owners_id = bob.id 
+FROM (SELECT id FROM owners WHERE owners.full_name='Bob') AS bob
+WHERE animals.name = 'Devimon' OR animals.name='Plantmon';
+SELECT * FROM animals;
+
+UPDATE animals
+SET owners_id = melody.id 
+FROM (SELECT id FROM owners WHERE owners.full_name='Melody Pond') as melody
+WHERE animals.name = 'Charmander' OR animals.name='Squirtle' OR animals.name='Blossom';
+SELECT * FROM animals;
+
+UPDATE animals 
+SET owners_id = dean.id 
+FROM (SELECT id FROM owners WHERE owners.full_name='Dean Winchester') as dean
+WHERE animals.name = 'Angemon' OR animals.name = 'Boarmon';
+SELECT * from animals;
+
+COMMIT;
+
+
+
 
 
 
